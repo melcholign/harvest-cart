30d616663c30a9e933891dcd58c1ccbf1ee6564e
melcholign <115886450+melcholign@users.noreply.github.com>
2024-11-20 19:46:19 +0600

    Change controller code from function to class

--- backend/controllers/customer-controller.js
+++ backend/controllers/customer-controller.js
@@ -1,25 +1,28 @@
 import { validationResult, matchedData } from 'express-validator';
 import bcrypt from 'bcrypt';
 import { formatValidationResult } from '../validators/formatters.js';
-import { Customer } from '../models/customer.model.js';
+import { CustomerModel } from '../models/customer.model.js';
 
-async function customerCreateController(req, res) {
-    const result = validationResult(req);
+class CustomerController {
 
-    if (!result.isEmpty()) {
-        const formattedResult = formatValidationResult(result);
-        res.status(400).json(formattedResult);
-        return;
+    static async createCustomer(req, res) {
+        const result = validationResult(req);
+
+        if (!result.isEmpty()) {
+            const formattedResult = formatValidationResult(result);
+            res.status(400).json(formattedResult);
+            return;
+        }
+
+        const data = matchedData(req);
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+
+        await CustomerModel.create({ ...data, hashedPassword });
+
+        res.sendStatus(200);
     }
-
-    const data = matchedData(req);
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-
-    await Customer.create({ ...data, hashedPassword });
-
-    res.sendStatus(200);
 }
 
 export {
-    customerCreateController,
+    CustomerController,
 }
--- backend/routes/customer-router.js
+++ backend/routes/customer-router.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import createCustomerValidator from '../validators/customer/customer-validator.js';
-import { customerCreateController } from '../controllers/customer-controller.js';
+import { CustomerController } from '../controllers/customer-controller.js';
 
 const router = express.Router();
 
-router.post('/create', createCustomerValidator(), customerCreateController);
+router.post('/create', createCustomerValidator(), CustomerController.createCustomer);
 
 export default router;
