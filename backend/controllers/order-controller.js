58a7406a80011b1fba3c7a5b6fc96db6dae809c5
melcholign <115886450+melcholign@users.noreply.github.com>
2024-12-01 15:32:42 +0600

    Integrate order services into order controllers

--- backend/controllers/order-controller.js
+++ backend/controllers/order-controller.js
@@ -1,11 +1,22 @@
 import { pool } from '../db/pool.js';
 import { OrderModel } from '../models/order-model.js';
+import { OrderService } from '../services/order-service.js';
 
 class OrderController {
 
     static async placeOrder(req, res) {
         const { customerId } = req.user;
-        //... logic from validating checkout and creating order
+
+        try {
+            await OrderService.createOrder(customerId);
+        } catch (err) {
+            return res.status(400).json({
+                error: {
+                    message: err.message,
+                }
+            });
+        }
+
         res.status(201).json({});
     }
 
@@ -13,8 +24,11 @@ class OrderController {
         const { customerId } = req.user;
 
         const orders = await OrderModel.getOrdersByCustomer(pool, customerId);
+        const updatedOrders = await OrderService.updateOrders(orders);
 
-        return res.status(200).json(orders);
+        return res.status(200).json({
+            data: updatedOrders,
+        });
     }
 
     static async getOrder(req, res) {
@@ -29,6 +43,10 @@ class OrderController {
             });
         }
 
-        return res.status(200).json(order);
+        const updatedOrder = await OrderService.updateOrders([order]);
+
+        return res.status(200).json({
+            data: updatedOrder,
+        });
     }
 }
