88a441c89281d858e393f7613f11808f39170fa6
melcholign <115886450+melcholign@users.noreply.github.com>
2024-12-01 10:16:53 +0600

    create an order service to keep order up to date

--- backend/services/order-service.js
+++ backend/services/order-service.js
@@ -1,6 +1,7 @@
 import { pool } from '../db/pool.js';
 import { OrderModel } from '../models/order-model.js';
 import { CheckoutModel } from '../models/checkout-model.js';
+import { OrderSimulationModel } from '../models/order-simulation-model.js';
 
 class OrderService {
 
@@ -14,10 +15,64 @@ class OrderService {
             checkout.shippingAddress, checkout.amount, checkout.items
         );
 
+        await OrderSimulationModel.createOrderSimulation(connection, orderId);
+
         await connection.commit();
         await connection.release();
 
         return orderId;
+    }
+
+    static async updateOrders(orders) {
+        const connection = await pool.getConnection();
+        await connection.beginTransaction();
+
+        for (let order of orders) {
+            const orderSimulation = await OrderSimulationModel.getOrderSimulation(
+                connection, order.orderId
+            );
+
+            if (
+                (order.orderStatus !== 'delivered')
+                && orderSimulation.deliveryDate <= Date.now()
+            ) {
+                await OrderModel.updateOrderStatus(
+                    connection, order.orderId, 'delivered',
+                )
+
+                order.orderStatus = 'delivered';
+
+                if (order.estimatedDeliveryDate === null) {
+                    await OrderModel.updateEstimatedDeliveryDate(
+                        connection, order.orderId, orderSimulation.deliveryDate
+                    );
+
+                    order.estimatedDeliveryDate = orderSimulation.deliveryDate;
+                }
+            }
+
+            if (
+                order.orderStatus === 'processing'
+                && orderSimulation.outForDeliveryDate <= Date.now()
+            ) {
+                await OrderModel.updateOrderStatus(
+                    connection, order.orderId, 'out for delivery'
+                );
+
+                order.orderStatus = 'out for delivery';
+
+                await OrderModel.updateEstimatedDeliveryDate(
+                    connection, order.orderId, orderSimulation.deliveryDate
+                );
+
+                order.estimatedDeliveryDate = orderSimulation.deliveryDate;
+            }
+        }
+
+        await connection.commit();
+        await connection.release();
+
+        return orders;
     }
 }
 
